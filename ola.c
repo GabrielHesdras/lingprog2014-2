e45a325df86327c568f1b53ebe7f3925c7402a3f
GabrielHesdras <158631744+GabrielHesdras@users.noreply.github.com>
2024-10-05 00:22:01 +0000

    Geek Nerd

--- ola.c
+++ ola.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 void main() {
-    printf("Olá Mundo!");
+    int x = 0; //criando uma variável inteira
+    printf("Valor de x: %d\n" ,x);
+    x++; //incremento x em 1 unidade
+    printf("Valor de x: %d\n" ,x);
+    float y = 12.345678; //criando uma variável Real
+    printf("Valor de y: %f\n" ,y);
+    //Imprimindo o valor de y com apenas 2 casas
+    printf("Valor de y: %.2f\n" ,y);
+    //1 Caractere deve estar entre apóstrofo
+    char sexo = 'M'; //variável que aceita 1 caractere
+    printf("Sexo: %c\n" , sexo);
+    //Criando uma variável String (Texto)
+    char nome[30]="João";
+    printf("Nome: %s\n",nome);
+    //Imprimindo a primeira letra do nome
+    printf("Primeira Letra: %c\n", nome[0]);
+    printf("Olá Mundo");
 }
